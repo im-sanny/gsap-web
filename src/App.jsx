4370526ec4f09d088e20250eed06e4d3308d9dbc
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-06-30 23:12:53 +0600

    remove: removed some codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,9 @@
-import NavBar from "./components/NavBar";
-import HeroSection from "./sections/HeroSection";
-import { ScrollSmoother, ScrollTrigger } from "gsap/all";
-import gsap from "gsap";
-import MessageSection from "./sections/MessageSection";
-import FlavorSection from "./sections/FlavorSection";
-import { useGSAP } from "@gsap/react";
-import NutritionSection from "./sections/NutritionSection";
-import BenefitSection from "./sections/BenefitSection";
-import TestimonialSection from "./sections/TestimonialSection";
-import FooterSection from "./sections/FooterSection";
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+import React from 'react'
 
 const App = () => {
-  useGSAP(() => {
-    ScrollSmoother.create({
-      smooth: 3,
-      effects: true,
-    });
-  });
-
   return (
-    <main>
-      <NavBar />
-      <div id="smooth-wrapper">
-        <div id="smooth-content">
-          <HeroSection />
-          <MessageSection />
-          <FlavorSection />
-          <NutritionSection />
-
-          <div>
-            <BenefitSection />
-            <TestimonialSection />
-          </div>
-
-          <FooterSection />
-        </div>
-      </div>
-    </main>
-  );
-};
+    <div>App</div>
+  )
+}
 
 export default App;
